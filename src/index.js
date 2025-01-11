ee6ebdbe8e5c96a8346e0a89960c2cc3845664d7
Waqas Khan <me@waqaskhan.dev>
2025-01-10 21:29:32 -0500

    final

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function Header() {
   // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
   return (
     <header className="header">
-      <h1>Fast React Pizza</h1>
+      <h1>Fast React Pizza Co.</h1>
     </header>
   );
 }
